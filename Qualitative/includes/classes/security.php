4e08d29097d1f2ef6c39e39f7cf555c31280d3dd
Aaron <Aaron@host67-126.vpn.ubc.ca>
2020-08-13 16:27:59 -0700

    change comments

--- Qualitative/includes/classes/security.php
+++ Qualitative/includes/classes/security.php
@@ -58,7 +58,7 @@ class Security
 
 	function ldap_login($p_userId, $p_pwd) {
 		$ldap = LDAP_LOGIN_HOST;
-		// TODO: change dn
+		// TODO: change dn - path to students cwl user
 		$usr = "uid=".$p_userId.",ou=People,dc=landfood,dc=ubc,dc=ca";
 		$ds = ldap_connect($ldap);
 		ldap_set_option($ds, LDAP_OPT_NETWORK_TIMEOUT, 3);
